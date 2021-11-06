56b46b660c7c1d909baf73a20139e2b5cc2632bb
di-nethra <anjana.dinethra@gmail.com>
2021-11-06 18:13:52 +0530

    navBar added

---
+++ src/pages/UserLandingPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PrimarySearchAppBar from "../components/AppBar";
+
+export default function UserLandingPage() {
+  return (
+    <div>
+      <PrimarySearchAppBar />
+    </div>
+  );
+}
